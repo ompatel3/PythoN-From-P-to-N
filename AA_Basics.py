f97197e78af7675ff7ab4068d8fbe91521d6f03a
Arpit tiwari <38579437+arpitiiitv@users.noreply.github.com>
2019-10-01 10:52:54 +0530

    Basic python codes

---
+++ AA_Basics.py
@@ -0,0 +1,38 @@
+## printing statments
+print("Hello world")
+print("Arpit Tiwari ",end="")
+print("Python Coder")
+# by default end="\n" newline
+print("Arpit Tiwari ",end=",")
+print("Python Coder",end=" and ")
+print("Good boy")
+print("Arpit","tiwari")
+
+
+#### printing special characters
+print("special characters")
+print("C:\network") # wrong
+print("C:\\network") # right
+print("\"Arpit\"")
+
+######
+print("typechecking")
+var1 = 100
+var2 = 100.12
+var3 = "100" 
+print(type(var2))
+print(type(var2))
+print(type(var3))
+
+##### Typecasting
+print("Typecasting")
+x= 100
+y= 200
+print(x+y)
+print(str(x)+str(y))
+print(float(x)+float(y))
+strng = "123"
+print(int(strng))
+
+### extra
+print(10*"Arpit")
---
+++ AB_input.py
@@ -0,0 +1,13 @@
+print("Enter Something")
+var = input()   # input will be taken as string
+print(type(var),var)
+
+print("Enter number")
+x=int(input())
+print(type(x),x)
+
+
+print("Enter a number to double")
+t=input()
+print(t*2) ### repeated number
+print(int(t)*2)  # typecast string into int
